ddcdcb430d3673b273a8e83676c319a22674c3ce
m-gb <maya.goldberg90@gmail.com>
2019-05-14 17:35:26 +0300

    Updates

---
+++ precache-manifest.41840948b603312f2159baf5354df5ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "95491b67539ad5635c96",
+    "url": "/starry-night/static/js/runtime~main.a7bdaaa1.js"
+  },
+  {
+    "revision": "6f5a5e8b4f8bd06b06fe",
+    "url": "/starry-night/static/js/main.910972b7.chunk.js"
+  },
+  {
+    "revision": "28ea905f5ddc7035b101",
+    "url": "/starry-night/static/js/2.a347797c.chunk.js"
+  },
+  {
+    "revision": "6f5a5e8b4f8bd06b06fe",
+    "url": "/starry-night/static/css/main.ee5c597f.chunk.css"
+  },
+  {
+    "revision": "28ea905f5ddc7035b101",
+    "url": "/starry-night/static/css/2.a8f8bc36.chunk.css"
+  },
+  {
+    "revision": "b6e9abc1d3dbb8f60bd4c3e9e011467e",
+    "url": "/starry-night/index.html"
+  }
+];
